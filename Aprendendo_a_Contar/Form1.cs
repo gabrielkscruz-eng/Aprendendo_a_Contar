c9f487fb1373a9534c7dc688d846aef259fe2334
Gabriel Kevin Souza Cruz <gabrielkscruz@gmail.com>
2025-11-03 11:41:12 -0300

    Adicionar arquivos de projeto.

---
+++ Aprendendo_a_Contar/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aprendendo_a_Contar
+{
+    public partial class jogo_contagem : Form
+    {
+        public jogo_contagem()
+        {
+            InitializeComponent();
+        }
+    }
+}
